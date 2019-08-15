32fbf7dcf7fd6ec5fa7a7da1b96688626ec17539
Felipe <felipe31soares@gmail.com>
2019-08-15 05:46:51 -0300

    Adicionada tela carrinho

--- src/com/dogsplaces/controller/SaleBean.java
+++ src/com/dogsplaces/controller/SaleBean.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 
+import com.dogsplaces.dao.DogDao;
+import com.dogsplaces.model.Dog;
 import com.dogsplaces.model.ItemSale;
 import com.dogsplaces.session.SessionContext;
 
@@ -12,47 +14,46 @@ import com.dogsplaces.session.SessionContext;
 public class SaleBean {
 	
 	
-	public String addSale() {
-		System.out.println("sdfasdf");
-		int i = 3;
+	public String addSale(ItemSale itemSale) {
+		
 		List<ItemSale> itemsSale;
 		if (SessionContext.getInstance().getItemsSale() == null) {
 			itemsSale = new ArrayList<ItemSale>();
-			System.out.println("primeira sessao");
 		} else {
 			itemsSale = SessionContext.getInstance().getItemsSale();
-			System.out.println("tem sessao");
-			
 		}
-		ItemSale itemSale = new ItemSale();
-		itemSale.setDogId(i);
-		itemSale.setSaleId(i);
-		itemsSale.add(itemSale);
-		SessionContext.getInstance().setAttribute("itemsSale", itemsSale);
-			
-		return "checkout?faces-redirect=true";
-	}
-	
-	public String addSale(int e) {
-		System.out.println("sdfasdf");
-		int i = 3;
-		List<ItemSale> itemsSale;
-		if (SessionContext.getInstance().getItemsSale() == null) {
-			itemsSale = new ArrayList<ItemSale>();
-			System.out.println("primeira sessao");
-		} else {
-			itemsSale = SessionContext.getInstance().getItemsSale();
-			System.out.println("tem sessao");
-			
-		}
-		ItemSale itemSale = new ItemSale();
-		itemSale.setDogId(i);
-		itemSale.setSaleId(i);
+//		ItemSale itemSale = new ItemSale();
 		itemsSale.add(itemSale);
 		SessionContext.getInstance().setAttribute("itemsSale", itemsSale);
 		return "";
 	}
+	private List<Dog> dogs = null;
 	
+	public List<Dog> getDogs() {
+		if (dogs != null) return dogs;
+		
+		List<ItemSale> itemsSale;
+		if (SessionContext.getInstance().getItemsSale() == null) {
+			itemsSale = new ArrayList<ItemSale>();
+		} else {
+			itemsSale = SessionContext.getInstance().getItemsSale();
+		}
+		
+		
+		dogs = new ArrayList<Dog>();
+		DogDao dogDao = new DogDao();
+		for (ItemSale itemSale : itemsSale) {
+			dogs.add(dogDao.getDog(itemSale.getDogId()));
+		}
+
+//		dogs.add(new Dog());		
+//		dogs.add(new Dog());		
+//		dogs.add(new Dog());		
+//		dogs.add(new Dog());
+		
+		return dogs;
+		
+	}
 	
 	
 //	private SaleDao dao;
