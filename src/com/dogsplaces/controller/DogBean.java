cca3a53d263e379b2c0d1c61ec71805e981d8535
Carlos Abade <carloseabade@gmail.com>
2019-08-14 23:19:22 -0300

    Teste do controller

---
+++ src/com/dogsplaces/controller/DogBean.java
@@ -0,0 +1,17 @@
+package com.dogsplaces.controller;
+
+public class DogBean {
+//	private DogDao dao = new DogDao();
+
+//	public Dog getDog() {
+//		return dao.select(); Select em um catioro
+//	}
+
+//	public void setDog(Dog dog) {
+//		this.dao.update(dog); Update em um catioro
+//	}
+	
+//	public List<Dog> getDogs() {
+//		return dao.selectAsteriscoFromDog()
+//	}
+}
---
+++ src/com/dogsplaces/controller/SaleBean.java
@@ -0,0 +1,37 @@
+package com.dogsplaces.controller;
+
+public class SaleBean {
+//	private SaleDao dao;
+//
+//	public Sale getSale() {
+//		return dao.select();
+//	}
+//
+//	public void setSale(Sale sale) {
+//		this.dao.update(sale);
+//	}
+//	
+//	public void insertItem(Dog dog) {
+//		this.dao.insert(dog);
+//	}
+//	
+//	public void removeItem(Dog dog) {
+//		this.dao.remove(dog);
+//	}
+//	
+//	public float getTotalPrice() {
+//		float totalPrice = 0;
+//		for (Dog dog : this.dao.getListItemSaleDao) {
+//			totalPrice += dog.getPrice();
+//		}
+//		return totalPrice;
+//	}
+//	
+//	public int getTotalItens() {
+//		int totalItens = 0;
+//		for (Dog dog : this.dao.getListItemSaleDao) {
+//			totalPrice += 1;
+//		}
+//		return totalItens;
+//	}
+}
